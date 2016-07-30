f79e290e962b1d3de269502ee25ab62764112c2a
shupac800 <davidshuman@gmail.com>
2016-07-30 18:06:12 -0500

    added CSS for page select

--- app.js
+++ app.js
@@ -1,47 +1,43 @@
-var app = angular.module("pag-test", ["ngRoute"]).controller("pagCtrl", foo);
+angular.module("pag-test", ["ngRoute"]).controller("pagCtrl", foo);
 
 function foo($scope,$route) {
-  var howMany = 3;
+  var rowsPerPage = 3;
   $scope.page = 1;
+  $scope.firstIndex = ($scope.page - 1) * rowsPerPage;
 
   var arr = [ {lname: "Anderson", docket: "012345"},
-              {lname: "Bailey", docket: "012574"},
-            	{lname: "Collins", docket: "012573"},
-            	{lname: "Diedrich", docket: "012141"},
+              {lname: "Buttafuoco", docket: "012574"},
+              {lname: "Collins", docket: "012573"},
+              {lname: "Diedrich", docket: "012141"},
               {lname: "Elkington", docket: "234781"},
               {lname: "Farnell", docket: "057544"},
               {lname: "Guthrie", docket: "123145"},
               {lname: "Holmes", docket: "098441"} ];
 
-  arr.sort(function(a, b) {
-    return b.docket - a.docket;
-  });
+  arr.sort(function(a, b) {return b.docket - a.docket});
   // redo this map after every sort
-  arr = arr.map(function(a,index) {let b=a;b.index=index;return b});
+  $scope.rows = arr.map(function(a,index) {let b=a;b.index=index;return b});
 
-  var numPages = Math.floor(arr.length / howMany) + 1;
+  var numPages = Math.floor(arr.length / rowsPerPage) + 1;
 
   // http://stackoverflow.com/questions/3746725/create-a-javascript-array-containing-1-n
   $scope.pages = Array.apply(null, {length: numPages}).map(Number.call, Number).map(n => n + 1);
-  
-  $scope.rows = arr;
-
-  $scope.firstIndex = ($scope.page - 1) * howMany;
+  $scope.pcolor = Array.apply(null, {length: numPages}).map(x => "pn");
+  $scope.pcolor[0] = "pn selected";
 
   $scope.myFilter = function() {
     return function(i) {
       return (i.index >= $scope.firstIndex) &&
-             (i.index < $scope.firstIndex + howMany);
+             (i.index < $scope.firstIndex + rowsPerPage);
     }
   }
 
   $scope.goToPage = function(p) {
+    $scope.pcolor = Array.apply(null, {length: numPages}).map(x => "pn");
+    $scope.pcolor[p] = "pn selected";
     $scope.page = p + 1;
-    $scope.firstIndex = ($scope.page - 1) * howMany;
+    $scope.firstIndex = ($scope.page - 1) * rowsPerPage;
     $route.reload();
   }
 
-  $scope.dynamicOrderFunction = function() {
-    return "-docket";
-  }
 }
